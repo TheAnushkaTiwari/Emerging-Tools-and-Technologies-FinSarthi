9956185dd2646723e179352d0c7cd816241e6c95
Anushka Tiwari <anushkatiwari8197@gmail.com>
2026-03-23 19:10:33 +0530

    Updated the code to facilitate front end connection and better performance

--- test_chat.py
+++ test_chat.py
@@ -6,18 +6,18 @@ url = "http://127.0.0.1:8000/api/chat/ask/"
 # A question that should be in your RBI PDF
 question = "What is investment?"
 
-print(f"🤖 Asking FinSarthi: '{question}'...")
+print(f"Asking FinSarthi: '{question}'...")
 
 try:
     response = requests.post(url, json={"message": question})
     
     if response.status_code == 200:
-        print("\n✅ SUCCESS!")
-        print("🤖 Bot Answer:", response.json().get("answer"))
-        print("📄 Sources:", response.json().get("sources"))
+        print("\nSUCCESS!")
+        print("Bot Answer:", response.json().get("answer"))
+        print("Sources:", response.json().get("sources"))
     else:
-        print("\n❌ Error:", response.status_code)
+        print("\nError:", response.status_code)
         print(response.text)
 
 except Exception as e:
-    print(f"\n❌ Connection Failed: {e}")
+    print(f"\nConnection Failed: {e}")
